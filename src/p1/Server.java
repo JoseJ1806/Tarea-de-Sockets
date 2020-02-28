4dbcefd1892369f74481b95b00138a641e7686ea
JoseJ1806 <49073450+JoseJ1806@users.noreply.github.com>
2020-02-28 12:47:03 -0600

    Interfaz del usuario

---
+++ src/p1/Server.java
@@ -0,0 +1,30 @@
+package p1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+	public static void main (String[] args) {
+		boolean active = true;
+		try {
+			int port = 40001;
+			ServerSocket serverSocket = new ServerSocket (port);
+			while (active) {
+				System.out.println("listening...");
+				Socket entrante = serverSocket.accept();
+				
+				BufferedReader lector = new BufferedReader(new InputStreamReader(entrante.getInputStream()));
+				String mensaje = lector.readLine();
+				System.out.println ("Mensaje recibido:" + mensaje);
+				entrante.close();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+				
+		}
+	}
+
+}
